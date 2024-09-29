2dd7a05000d966813d1a24f2231b652b896771c9
Quang Pham <quangpham1019@gmail.com>
2024-09-29 16:58:17 -0700

    Add MVC support.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@ namespace EduOnline
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
