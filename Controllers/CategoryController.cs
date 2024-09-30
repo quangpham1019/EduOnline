de37b8a78714984647d58f6ded33872c66639bf1
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-09-30 12:40:56 -0700

    Use tag helpers and Model Binding to create an html form.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace AspDOTNET_MVC.Controllers
         // Optional parameter is one way to handle improper input
         public IActionResult Edit(int? id)
         {
-            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            var category = CategoryRepository.GetCategoryById(id.HasValue ? id.Value : 0);
 
             return View(category);
         }
