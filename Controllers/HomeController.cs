3a38ea72a2786501bdd34d485a6d636385e6b37f
Quang Pham <quangpham1019@gmail.com>
2024-09-29 17:06:19 -0700

    Add Action Method (Request Handlers).

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // Action == Request Handler's Name
+        public string Index()
+        {
+            return "Hello World from Action Method";
+        }
+
+        public string Error()
+        {
+            return "I have an error here.";
+        }
+    }
+}
