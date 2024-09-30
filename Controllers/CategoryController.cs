a4894d54fddd60a168f2d8d44f06877a6561f53e
Quang Pham <quangpham1019@gmail.com>
2024-09-29 18:00:12 -0700

    Pass models to views.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduOnline.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduOnline.Controllers
 {
@@ -13,13 +14,9 @@ namespace EduOnline.Controllers
         // Optional parameter is one way to handle improper input
         public IActionResult Edit(int? id)
         {
-            if (id.HasValue)
-            {
-                return new ContentResult { Content = id.ToString() };
-            } else
-            {
-                return new ContentResult { Content = "null content" };
-            }
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+
+            return View(category);
         }
     }
 }
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace EduOnline.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
